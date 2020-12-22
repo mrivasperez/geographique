9cad37ee22baf0912683f1ba268c32489863d2b2
Miguel Rivas Perez <mrivasperez1996@gmail.com>
2020-12-21 17:34:19 -0800

    added styling

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 
 import "./MainNavigation.css";
 import MainHeader from "./MainHeader";
+import NavLinks from "./NavLinks";
 
 const MainNavigation = (props) => {
   return (
@@ -14,7 +15,9 @@ const MainNavigation = (props) => {
       <h1 className="main-navigation__title">
         <Link to="/">Geographique</Link>
       </h1>
-      <nav>...</nav>
+      <nav>
+        <NavLinks />
+      </nav>
     </MainHeader>
   );
 };
