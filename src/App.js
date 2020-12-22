2fe8c9455360c229e5d0e55b8008dc80695b323f
Miguel Rivas Perez <mrivasperez1996@gmail.com>
2020-12-21 17:07:42 -0800

    added new components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
 
 const App = () => {
   return (
     <Router>
-      <Route path="/" component={Users} exact />
+      <Switch>
+        <Route path="/" component={Users} exact />
+        <Route path="/places/new" component={NewPlace} exact />
+        <Redirect to="/" />
+      </Switch>
     </Router>
   );
 };
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,5 +1,17 @@
+import UsersList from "../components/UsersList";
+
 const Users = (props) => {
-  return <h2>Users Works!</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Mikayla",
+      image:
+        "https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=80",
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
