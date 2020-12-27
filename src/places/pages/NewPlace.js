326adc0171b3e506e3b7913ed72cdc9d0030456b
Miguel Rivas Perez <mrivasperez1996@gmail.com>
2020-12-27 13:12:24 -0800

    update

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Input from "../../shared/components/FormElements/Input";
 
+import "./NewPlace.css";
+
 const NewPlace = () => {
   return (
     <form className="place-form">
-      <Input type="text" label="Title" />
+      <Input type="text" label="Title" element="input" validators={[]} />
     </form>
   );
 };
